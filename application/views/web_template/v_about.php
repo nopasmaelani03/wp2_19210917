5dcd089e0300abf8f1c5d80b63499f389c518120
nopasmaelani03 <dwimaelaniinova@gmail.com>
2022-10-03 23:06:18 -0700

    Membuat Templating Website Sederhana

---
+++ application/views/web_template/v_about.php
@@ -0,0 +1,23 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+<h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : keona anak</li>
+ <li>Nama Belakang : monyet</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li> katanya mah di jonggol apaya</li>
+ </ul>
+ 
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+ <li>pinggir jalan apaya</li>
+ </ul>
+ <h4>Hobi Favorit</h4>
+ <ul type="square">
+ <li>mainin perasaan cewe</li>
+ <li>pemberi harapan palsu</li>
+ </ul>
+</section>
