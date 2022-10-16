44bbc4b4b5c52c2d782a9b73667152c9083f29f0
nopasmaelani03 <dwimaelaniinova@gmail.com>
2022-10-16 01:36:52 -0700

    matakuliah

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array('url');
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array();
+$autoload['libraries'] = array('form_validation');
 
 /*
 | -------------------------------------------------------------------
