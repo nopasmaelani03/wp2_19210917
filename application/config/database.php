052e9c653a19247c62e631dfbd207758a952d438
nopasmaelani03 <dwimaelaniinova@gmail.com>
2022-11-12 06:43:57 -0800

    Pustaka Booking - Membuat Autentifikasi dan Home Admin

--- application/config/database.php
+++ application/config/database.php
@@ -25,7 +25,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |				 odbc, pdo, postgre, sqlite, sqlite3, sqlsrv
 |	['dbprefix'] You can add an optional prefix, which will be added
 |				 to the table name when using the  Query Builder class
-|	['pconnect'] TRUE/FALSE - Whether to use a pesrsistent connection
+|	['pconnect'] TRUE/FALSE - Whether to use a persistent connection
 |	['db_debug'] TRUE/FALSE - Whether database errors should be displayed.
 |	['cache_on'] TRUE/FALSE - Enables/disables query caching
 |	['cachedir'] The path to the folder where cache files should be stored
